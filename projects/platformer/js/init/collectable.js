95c4903b8ec3fbebe79db46f97c05c8bae8b349b
Ubuntu <ubuntu@ip-172-31-46-206.ec2.internal>
2020-07-14 19:53:36 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/collectable.js
@@ -0,0 +1,53 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let collectable = window.opspark.collectable;
+
+    let type = {
+        db: {assetKey: 'db', points: 10},
+        max: {assetKey: 'max', points: 20},
+        steve: {assetKey: 'steve', points: 30},
+        grace: {assetKey: 'grace', points: 40},
+        kennedi: {assetKey: 'kennedi', points: 50}
+    };
+    
+    /**
+     * init: Initialize all collectables.
+     * 
+     * GOAL: Add as many collectables as necessary to make your level challenging.
+     * 
+     * Use the createCollectable() Function to create collectables for the level.
+     * See the type Object, above, for the types of collectables and their point values.
+     * 
+     * createCollectable() takes these arguments:
+     *      
+     *      createCollectable(type, x, y, gravity, bounce);
+     * 
+     *      type: The type of the collectable, use the type Object above.
+     *      x: The x coordineate for the collectable.
+     *      y: The y coordineate for the collectable.
+     *      gravity: OPTIONAL The gravitational pull on the collectable.
+     *      bounce: OPTIONAL A factor effecting how much the collectable will bounce off platforms, etc.
+     */ 
+    function init(game) {
+        let createCollectable = collectable.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        createCollectable(type.db, 400, 300, 1, 3);
+        createCollectable(type.max, 100, 10, 4, 0.5);
+        createCollectable(type.steve, 100, 200, 7, 1);
+        createCollectable(type.kennedi, 600, 10, 4, 1);
+        createCollectable(type.grace, 400, 100, 4, 1.2);
+        createCollectable(type.grace, 750, 500, 4, 1.2);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+    collectable.init = init;
+})(window);
